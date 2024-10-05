746fab811a5bcde0e950fe370156d5314ccf5d6f
piyushgaikwad0205 <85379886+piyushgaikwad0205@users.noreply.github.com>
2024-10-05 13:33:34 +0530

    new

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
-model = load_model('ff_Xception.h5')
+model = load_model('celebs_Xception.h5')
 
 def predict_image(model, image_path):
     img = cv2.imread(image_path)
@@ -11,13 +11,14 @@ def predict_image(model, image_path):
     img = img / 255.0
     img = np.expand_dims(img, axis=0)
     prediction = model.predict(img)
-    if prediction[0] > 0.5:
+    prediction_value = prediction[0][0] 
+    if prediction_value > 0.5:
         print("The image is a deepfake.")
     else:
         print("The image is real.")
 
 def main():
-    image_path = 'image.png'
+    image_path = 'Screenshot 2024-10-05 125418.png'
     predict_image(model, image_path)
 
 if __name__ == "__main__":
